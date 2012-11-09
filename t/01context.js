8c88183777665edaf822eff880fb491e0900761f
n4kz <nfokz@yandex.ru>
2012-11-09 21:22:53 +0000

    Emit auto event in async way

--- t/01context.js
+++ t/01context.js
@@ -16,38 +16,40 @@ Controller.prototype.emit = function (event) {
 	events[event]++;
 }
 
-module.exports = {
-	'Context()': function () {
-		var controller = new Controller(),
-			request = {
-				method: 'GET',
-				url: '/test?param=5&format=unknown',
-				headers: {
-					host: 'test.localhost'
-				},
-				connection: {
-					remoteAddress: '127.0.0.1'
-				}
+module.exports.testAsync = function (beforeExit, assert) {
+	var controller = new Controller(),
+		request = {
+			method: 'GET',
+			url: '/test?param=5&format=unknown',
+			headers: {
+				host: 'test.localhost'
 			},
-			response = {},
-			context = new Context(controller, request, response);
+			connection: {
+				remoteAddress: '127.0.0.1'
+			}
+		},
+		response = {},
+		context = new Context(controller, request, response);
 
-		/* Context */
-		assert(context instanceof Controller);
-		assert.equal(context.emit, controller.emit);
-		assert.equal(context.req, request);
-		assert.equal(context.res, response);
-		assert.equal(context.method, request.method);
-		assert.equal(context.url, '/test');
+	/* Context */
+	assert.ok(context instanceof Controller);
+	assert.equal(context.emit, controller.emit);
+	assert.equal(context.req, request);
+	assert.equal(context.res, response);
+	assert.equal(context.method, request.method);
+	assert.equal(context.url, '/test');
+	assert.ok(context.is_get);
+	assert.ok(!context.is_post);
 
-		/* Params */
-		assert.type(context.params, 'object');
-		assert.equal(Object.keys(context.params).length, 2);
-		assert.equal(context.params.param, 5);
-		assert.equal(context.params.format, 'unknown');
+	/* Params */
+	assert.type(context.params, 'object');
+	assert.equal(Object.keys(context.params).length, 2);
+	assert.equal(context.params.param, 5);
+	assert.equal(context.params.format, 'unknown');
 
-		/* Auto event */
+	/* Auto event */
+	beforeExit(function () {
 		assert.equal(events.auto, 1);
 		assert.equal(Object.keys(events).length, 1);
-	}
+	});
 };
