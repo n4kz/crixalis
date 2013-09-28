3c1f5b02289bd0fc1e54c73dcf2bc1fc66583d79
n4kz <n4kz@n4kz.com>
2013-09-28 13:15:20 +0400

    Default route for static plugin

--- examples/viewer.js
+++ examples/viewer.js
@@ -1,44 +1,23 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
-var http = require('http'),
-	c    = require('crixalis'),
-	port = 3000,
-	root = './doc';
+var Crixalis = require('../lib/controller.js');
 
-c
+Crixalis
 	.plugin('static')
-	.plugin('compression')
+	.plugin('shortcuts')
+	.plugin('compression');
 
-c.expires = {
-	'application/javascript' : 8.64E+8,
-	'text/css'               : 8.64E+8,
-	'image/gif'              : 8.64E+9,
-	'image/png'              : 8.64E+9
-};
+Crixalis.staticPath = 'doc';
 
-c.on('auto', function () {
-	console.log('Got ' + this.method + ' request to ' + this.url + ' from ' + this.address);
-	this.select();
-});
-
-c.router({
-	async: true,
-	methods: ['GET', 'HEAD']
-}).from(/^(.*)$/).to(function () {
-	var file = root + '/' + this.params.$1
-		.replace(/^\/*/, '')
-		.replace(/\.\.\//g, '\/'),
-		that = this;
-
-	if (this.params.path === '/') {
-		file += 'index.html';
-	}
-
-	this.serve(file, function (error) {
-		if (error) {
-			that.error(error);
-		}
+Crixalis
+	.on('auto', function () {
+		console.log('Got ' + this.method + ' request to ' + this.url + ' from ' + this.address);
+		this.select();
+	})
+	.router()
+	.get('/', function () {
+		this.async = true;
+		this.serve(this.staticPath + '/index.html');
 	});
-});
 
-server = http.createServer(c.handler).listen(port);
+Crixalis.start('http', 3000);
